68eee89a8ec5e462d99502ef3f9eaab6c5faa7e2
anuragbhatt408 <anuragbhatt408@gmail.com>
2024-07-20 18:23:40 +0530

    add user registeration and validation

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,17 @@ import express, { NextFunction, Request, Response } from 'express';
 import createHttpError, { HttpError } from 'http-errors';
 import { config } from './config/config';
 import { globalErrorHandler } from './middlewares/globalErrorHandler';
+import userRouter from './user/userRouter';
 
 const app = express();
+app.use(express.json())
 
 // Routes
 app.get('/', (req, res) => {
     res.json({ message: 'hii from get' })
 })
+
+app.use('/api/users', userRouter);
 
 
 // Global Error Handlers
